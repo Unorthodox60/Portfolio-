c519e73317bfb90744ecd850e61ef5824882fb1f
Aryan Tripathi <aryantripathi198@gmail.com>
2026-03-19 21:11:39 +0530

    Add Navbar component

---
+++ Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h1>Portfolio</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
